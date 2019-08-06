708019c807f45840741de757e6a258c38b33e78c
Ian Harold <ianhunterharold@gmail.com>
2019-08-06 22:20:50 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,28 @@
-# Code your solution here!
+def run_guessing_game
+  # generte a random number 
+  random = rand(6) + 1 
+  random_as_string = random.to_s
+
+  #prompting user to guess with a string statement 
+  prompts_user_to_guess  = "Please guess any number between 1 through 6."
+  
+  print prompts_user_to_guess
+  
+  # input is whatever the user selects as their number 
+ 
+ input = (gets.chomp)
+  #capture user input from command line 
+  #remember that input is the value they typed in 
+  
+  if (input == "exit")
+      print "Goodbye!"
+  elsif (input !=  random_as_string)
+      print "Sorry! The computer guessed #{random}."
+  elsif (input == random_as_string)
+      print "You guessed the correct number!" 
+   end 
+end 
+
+#what did i learn, logic of your condition is important. If you only present your conditional with two options but you have a third, it will never go to the third option. so i organized to edge case or exit first then not working then working. 
+
+also learned that .to_s just returns the value as a string,so it needs to be assigned to a variable to be stored 
